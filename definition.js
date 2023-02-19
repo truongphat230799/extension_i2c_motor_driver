053020181b4854ebd6885a02ba721c99c0de78d4
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2023-02-20 01:03:00 +0700

    add file

---
+++ definition.js
@@ -0,0 +1,58 @@
+Blockly.Blocks['i2c_motor_driver'] = {
+    init: function () {
+      this.jsonInit(
+        {
+            "type": "i2c_motor_driver",
+            "message0": "quay động cơ %1 tốc độ %3 %2",
+            "args0": [
+              {
+                "type": "field_dropdown",
+                "name": "motor",
+                "options": [
+                  [
+                    "1",
+                    "MD4C_REG_CH1"
+                  ],
+                  [
+                    "2",
+                    "MD4C_REG_CH2"
+                  ],
+                  [
+                    "3",
+                    "MD4C_REG_CH3"
+                  ],
+                  [
+                    "4",
+                    "MD4C_REG_CH4"
+                  ]
+                ]
+              },
+              {
+                "type": "field_number",
+                "name": "speed",
+                "value": 0,
+                "min": -100,
+                "max": 100,
+                "precision": 50
+              },
+              {
+                "type": "input_dummy",
+              }
+            ],
+            "previousStatement": null,
+            "nextStatement": null,
+            "colour": "#cb2026",
+            "tooltip": "",
+            "helpUrl": ""
+          }
+      );
+        }
+    };
+
+    Blockly.Python['i2c_motor_driver'] = function(block) {
+        var dropdown_motor = block.getFieldValue('motor');
+        var number_speed = block.getFieldValue('speed');
+        // TODO: Assemble Python into code variable.
+        var code = 'set_motor(' + dropdown_motor + ','+ number_speed+ ')\n';
+        return code;
+      };
