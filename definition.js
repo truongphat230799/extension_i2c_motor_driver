e2a7fd09486936474a8a146d698ed1927d277343
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2023-02-20 11:04:58 +0700

    a

--- definition.js
+++ definition.js
@@ -49,9 +49,9 @@ Blockly.Blocks['i2c_motor_driver'] = {
     Blockly.Python['i2c_motor_driver'] = function(block) {
         Blockly.Python.definitions_['import_display'] = 'from yolobit import *';
         Blockly.Python.definitions_['import_motor_driver'] = 'from i2c_motor_driver import *';
-        var motor = Blockly.Python.valueToCode(block, 'motor', Blockly.Python.ORDER_ATOMIC);
+        var dropdown_motor = block.getFieldValue('motor');
         var speed = Blockly.Python.valueToCode(block, 'speed', Blockly.Python.ORDER_ATOMIC);
         // TODO: Assemble Python into code variable.
-        var code = 'set_motor(' + motor + ','+ speed+ ')\n';
+        var code = 'set_motor(' + dropdown_motor + ','+ speed+ ')\n';
         return code;
       };
